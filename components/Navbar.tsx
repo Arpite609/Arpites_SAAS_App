b287e7dba2ae28b3e3b70e35fd1e978b62b5ede3
“MsiAr” <snowflaker6090@gmail.com>
2025-06-13 16:36:29 +0530

    added navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import NavItems from "./NavItems";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <Link href="/" className="logo">
         <div className="flex items-center gap-2.5 cursor pointer ">
-          <Image src='/images/logo.svg' alt="navbar" />
+          <Image src='/images/logo.svg' alt="navbar" width={46} height={46} />
         </div>
       </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <p>Sign In</p>
+      </div>
     </nav>
   );
 };
